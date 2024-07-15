6e57ce1dd5c67bc3bddf0210d1e697650093fae5
Petr Losev <losev-mf@mail.ru>
2024-07-15 16:07:06 +0300

    Aside menu created.

---
+++ src/components/InventoryDetails.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="inventory-details">
+    <img src="@/assets/icons/inventoryLabel.png" alt="размытое изображение высоких башен на фоне земли и неба" />
+
+    <div class="inventory-details__text">
+      <h2></h2>
+
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+
+      <p></p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '@/assets/variables';
+
+.inventory-details {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 18px 14px;
+  outline: 1px solid $light-grey;
+  border-radius: 12px;
+  margin: 0 auto;
+
+  img {
+    max-width: calc(100);
+    margin: 0 0 20px;
+    background: transparent;
+  }
+
+  &__text {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 9px 6px;
+
+    &>* {
+      width: 100%;
+      background: $light-grey;
+    }
+
+    h2 {
+      padding: 13px;
+      margin: 0 0 24px;
+      border-radius: 8px;
+    }
+
+    p {
+      padding: 5px;
+      margin: 0 0 16px;
+      border-radius: 4px;
+
+      &:first-of-type {
+        width: calc(100% - 44px);
+      }
+
+      &:nth-of-type(3n) {
+        width: calc(100% - 30px);
+      }
+
+      &:nth-of-type(4n) {
+        width: calc(100% - 40px);
+      }
+
+      &:nth-of-type(5n) {
+        width: calc(100% - 60px);
+      }
+
+      &:last-of-type {
+        width: calc(100% - 120px);
+        margin: 8px 0 6px;
+      }
+    }
+  }
+}
+</style>
--- src/components/InventoryFooter.vue
+++ src/components/InventoryFooter.vue
@@ -1,3 +1,15 @@
 <template>
-  <div class="decription"></div>
+  <div class="decription">
+    <p></p>
+  </div>
 </template>
+
+<style lang="scss">
+@import '@/assets/variables';
+
+.decription {
+  padding: 18px;
+  outline: 1px solid $item-background;
+  border-radius: 12px;
+}
+</style>
